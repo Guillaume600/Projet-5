0ef73d7b016d11043a8df1bbd44582bd51010f78
Guillaume Sissan <guillaume.sissan.pro@gmail.com>
2022-06-16 18:40:22 +0200

    maj master

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,4 @@
-// récup identifiant produit depuis URL
-const urlProduct = new URL(document.location);
-//la propriété searchparams permet de récupérer des données d'une URL, en l'occurrence les ID
+const urlProduct = new URL(document.location); // récup identifiant produit depuis URL
 let params = urlProduct.searchParams;
 let id = params.get('id');
 
@@ -16,64 +14,72 @@ const colorsElementHtml = document.getElementById("colors");
 const urlApi = `http://localhost:3000/api/products/${id}`;
 
 const quantityElementHtml = document.getElementById("quantity");
-// pour s'assurer que l'élément apparait dans la console (et comment il apparait)
-console.log(urlApi);
-// récupérer contenu url
-fetch(urlApi)
+
+console.log(urlApi); // pour s'assurer que l'élément apparait dans la console (et comment il apparait) 
+fetch(urlApi) // récupérer contenu url
     .then(function(response) { // PUIS fonction DANS fonction 
         console.log(response);
         return response.json();
     })
     .then(function(data) {
         console.log(data); //données du produit
-        // ici, on complète le contenu des balises HTML directement par les données associées dans l'API (nom, prix, image...)
+
+        document.title = data.name;
         titleElementHtml.innerText = data.name;
         priceElementHtml.innerText = data.price;
         photoElementHtml.src = data.imageUrl;
         photoElementHtml.alt = data.altTxt;
         descriptionElementHtml.innerText = data.description;
-        //variable associée au choix des couleurs
         let txt = "<option value=\"\" disabled>--SVP, choisissez une couleur --</option>";
-        // création d'une boucle permettant de récupérer chacune des couleurs de chaque produit 
         for (let color of data.colors) {
             console.log(color);
             // txt += "<option value=\"" + color + "\">" + color + "</option>";
-            // les ${} permettent de récupérer les données de l'API et de les intégrer dans l'HTML
             txt += `<option value="${color}">${color}</option>`
         }
         console.log(txt);
         colorsElementHtml.innerHTML = txt;
     });
-/*Dans un premier temps, on crée l'interaction avec la balise addToCart (ajouter au panier) en récupérant la balise
-Puis on lui ajoute la fonction addEventListener du clic */
+
+quantityElementHtml.addEventListener("input", function() {
+    let quantityIsValid = quantityElementHtml.value <= 100;
+    const errorMessage = document.getElementById("quantityErrorMessage");
+    if (quantityIsValid) {
+        errorMessage.innerText = "";
+    } else {
+        errorMessage.innerText = "Quantité max autorisée : 100";
+    }
+});
+
 document.getElementById("addToCart").addEventListener("click", function(evt) {
     console.log(evt.target.textContent);
-    // transformation quantity de string à nombre
-    let quantityString = quantityElementHtml.value;
-    // transformer string en nombres entiers
+    let quantityString = quantityElementHtml.value; // transformation quantity de string à nombre
     let quantity = Number.parseInt(quantityString, 10);
     let colors = colorsElementHtml.value;
+    let validationMessageHtml = document.getElementById("validationPanier");
+    let erreurPanierHtml = document.getElementById("erreurPanier");
     console.log(id);
     console.log(colors);
     console.log(quantity);
 
-    // récup contenu storage
-    let panierStringOriginal = window.localStorage.getItem("panier");
-    if (panierStringOriginal == null) {
-        //si pas de panier = création panier vide
+    let panierStringOriginal = window.localStorage.getItem("panier"); // récup contenu storage
+    if (panierStringOriginal == null) { //si pas de panier = création panier vide
         panierStringOriginal = "[]";
     }
     let panier = JSON.parse(panierStringOriginal); // conversion string json vers objet js
     console.log(panier);
+    validationMessageHtml.innerText = "";
+    erreurPanierHtml.innerText = "";
 
     //si produit existe déjà dans panier incrémentation quantité
     let productExisting = false;
     for (let product of panier) {
         if (product.id == id && product.colors == colors) { // si l'id ET la couleur snt déjà dans panier
-            if (product.quantity + quantity >= 100) {
-                product.quantity = 100;
-            } else {
+            if (product.quantity + quantity <= 100) {
                 product.quantity += quantity; // ajouter quantity à quantity déjà existante
+                validationMessageHtml.innerText = `${quantity} produit(s) ajouté(s) au panier`;
+            } else {
+                console.log("Produit pas ajouté car quantité trop grande");
+                erreurPanierHtml.innerText = "Produit pas ajouté car quantité trop grande";
             }
             productExisting = true;
         }
@@ -85,12 +91,11 @@ document.getElementById("addToCart").addEventListener("click", function(evt) {
             quantity: quantity,
             colors: colors,
         });
-    } else {
-        console.log("Produit pas ajouté car quantité trop grande");
+        validationMessageHtml.innerText = `${quantity} produit(s) ajouté(s) au panier`;
     }
 
     let panierString = JSON.stringify(panier);
-    //On ajoute les éléments ajoutés au panier directement le local storage du navigateur
     window.localStorage.setItem('panier', panierString);
     console.log(panier);
+    //ajouter alerte prévention 2nd clic si choix similaire
 });
